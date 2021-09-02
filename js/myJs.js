6dd1bda36763d06b58bc784aa00cc078a9377a16
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-02 19:09:29 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,5 +1,5 @@
 const textConfig = {
-  text1: "He luu cậu!",
+  text1: "Hello cậu!",
   text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
   text3: "Cậu yêu tớ có phải không nào ._.",
   text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
@@ -7,10 +7,10 @@ const textConfig = {
   text6: "Yêu ơi là yêu <3",
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try vlllll",
+  text9: "Vì cậu đẹp trai và dễ thương nhất",
   text10: "Tớ biết mà ^^ Yêu cậu 300.000",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    " :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo https://www.facebook.com/profile.php?id=100041664459280",
   text12: "Okii lunn <3",
 };
 
