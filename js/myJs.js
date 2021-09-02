66aefd095edb78a9a4569a852f361507ceb496b3
bawdibuisreal <64428460+bawdibuisreal@users.noreply.github.com>
2021-09-02 19:57:21 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -138,7 +138,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "http://fb.com";
+            window.location = "https://www.facebook.com/profile.php?id=100041664459280";
           },
         });
       }
